4d9dbb1eeea3b4f98be47d01c346b9c2a3aa39b4
Camila Silva <camilasilvabernal18@gmail.com>
2022-09-23 11:12:14 -0500

    Integre el carrito de comprar al navbar principal, tanto en version mobile como en desktop

--- main.js
+++ main.js
@@ -1,17 +1,56 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const burgerMenu = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarIcon = document.querySelector('.navbar-shopping-card');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+menuCarIcon.addEventListener('click', toggleAsideCar);
 
 function toggleDesktopMenu () {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    } 
+
     desktopMenu.classList.toggle('inactive');
-}
+
+    }
+
+
+    
+
 
 
 function toggleMobileMenu () {
+
+    /* cierra el carrito de compras cuando abro el menu */
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    /* Me abre y cierra el menu */
     mobileMenu.classList.toggle('inactive');
+
+}
+
+function toggleAsideCar () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');    
 }
